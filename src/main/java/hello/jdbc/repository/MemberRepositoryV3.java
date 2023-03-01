0e7933d3326749459be356d49e4bb85289299fe0
WJ_Kim <mw7895la@gmail.com>
2023-03-01 11:27:53 +0900

    [2023-03-01] 1. PlatformTransactionManager 사용 트랜잭션 동기화

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV3.java
@@ -0,0 +1,151 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ * DataSourceUtils.getConnection(); 을 커넥션 얻기 위해 사용한다.
+ * DataSourceUtils.releaseConnection(); 커넥션 닫을 때 이걸 쓴다.
+ * 트랜잭션 동기화 매니저에 접근해서  커넥션 가져오고 닫고 하는 코드들이다.
+ */
+@Slf4j
+public class MemberRepositoryV3 {
+
+    //스프링에는 기본적으로 DataSource의 구현체로 HikariCP가 빈으로 등록되어있고, 그것이 주입된다.
+    //여기서는 DataSource가 스프링 컨테이너가 구동 시 설정파일을 확인하고 자동으로 빈으로 생성한다.
+    private final DataSource dataSource;
+
+    @Autowired
+    public MemberRepositoryV3(DataSource dataSource) {      //주입을 받았다.
+        this.dataSource = dataSource;
+    }
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id,money) values(?,?)";
+
+        Connection con = null;      //Connection이 있어야 연결을 하지
+        PreparedStatement pstmt = null;     //DB에 쿼리를 날린다.      //PreparedStatement 인터페이스다 이것의 부모는 Statement다 // ?를 통한 파라미터 바인딩을 가능하게 해줌.
+        try{
+            con = getConnection();      //DriverManager를 통해서 커넥션을 획득하게 된다.
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1,member.getMemberId());  //여기는 파라미터 바인딩을 하는 곳 위의 (?, ?)  첫번째 자리의 index는 1
+            pstmt.setInt(2, member.getMoney());     // 2번째 자리의 index는 2
+            pstmt.executeUpdate();      // 위에 준비된게 DB에 실행이 된다. 얘가 숫자를 반환하는데  영향받은 row 수만큼 반환해준다.
+            return member; //반환 타입이 Member라서 리턴.
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally{
+            //여기서 사용한것을 close 해줘야 해. 시작과 역순으로 클로즈 해라.
+            //안닫으면, 지금 외부 리소스를 쓰는건데. 잘못하면 계속 떠다니면서 연결이 안끊어지고 유지가 된다.  리소스 누수로 인해 커넥션 부족으로 장애 발생할 수 있음.
+
+            //pstmt.close();      //근데 만약에 close에서  Exception이 터지면? 호출이 안되는 문제가 발생할 수 있다. 그래서 아래 close() 로 따로 뺴놨다.
+            //con.close();
+            close(con,pstmt,null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs  =null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1,memberId);
+            rs = pstmt.executeQuery();//executeUpdate 는 뭔가 테이블에 변경이 있을 insert delete update 같은것에 쓰고  단순 조회는 executeQuery()를 쓴다.
+            //반환은 ResultSet 이다. select 쿼리의 결과를 담고 있는 통.
+
+            if(rs.next()){      //처음에 cursor는 데이터를 가리키지 않다가 rs.next() 해줘야 첫번째 데이터를 가리킨다. 여기는 조회라서 조회는 어짜피 1개만 조회하는거라 if문을 썼다.
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        }catch(SQLException e){
+            log.error("db error",e);
+            throw e;
+        }finally{
+            close(con,pstmt,rs);
+        }
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1,money);  //여기는 파라미터 바인딩을 하는 곳 위의 (?, ?)  첫번째 자리의 index는 1
+            pstmt.setString(2, memberId);     // 2번째 자리의 index는 2
+            int resultSize=pstmt.executeUpdate();      // 위에 준비된게 DB에 실행이 된다. 얘가 숫자를 반환하는데  영향받은 row 수만큼 반환해준다.
+            log.info("resultSize ={}", resultSize);
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally{
+            close(con,pstmt,null);
+        }
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, memberId);     // 2번째 자리의 index는 2
+            int resultSize=pstmt.executeUpdate();      // 위에 준비된게 DB에 실행이 된다. 얘가 숫자를 반환하는데  영향받은 row 수만큼 반환해준다.
+            log.info("resultSize ={}", resultSize);
+        }catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        }finally{
+            close(con,pstmt,null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {      //항상 역순으로 close ()
+        
+        //기존 코드를 대체 내부를 봐라.
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+
+        //주의!! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다. -> 트랜잭션 동기화 매니저가 관리하는 커넥션이면 닫지않고 유지해준다. 관리 안하는 커넥션이면 닫아버린다.
+        DataSourceUtils.releaseConnection(con,dataSource);
+
+        //JdbcUtils.closeConnection(con);     //이렇게 하면 커넥션을 팍 꺼버린다. DB랑 연결이 다 끝나버린다. 세션도 사라지고..
+
+    }
+
+    private  Connection getConnection() throws SQLException {
+        //주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        //트랜잭션 동기화 매니저를 통해 보관된 커넥션을 얻는다.
+        //트랜잭션 동기화 매니저가 보관하는 커넥션이 있으면 그걸 반환, 없으면 생성해서 반환
+
+
+        log.info("get connection ={} class={}", con, con.getClass());
+        return con;
+    }
+}
---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,60 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final MemberRepositoryV3 memberRepository;
+    private final PlatformTransactionManager transactionManager;
+
+    //private final DataSource dataSource;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        //트랜잭션 시작.
+        //반환된 TransactionStatus는 커밋할때 넣어줘야함.
+        // V3_1Test에서 구현체인 DataSourceTransactionManager를 통해서 트랜잭션을 획득한다. 트랜잭션 매니저는 데이터소스를 알고 있다.  데이터소스를 통해서 커넥션을 생성, 셋오토커밋 펄스도 하고, 트랜잭션 동기화 매니저에 보관한다.
+        //그 다음에 로직을 수행하는데 findById() 를 수행한다 하자. 커넥션 획득하는데,어디서 획득? DataSourceUtils.getConnection(dataSource)하면  리포지토리에서 트랜잭션 동기화 매니저의 트랜잭션 시작한 커넥션을 획득하고 로직을 수행한다.
+        //로직을 다 수행하고 트랜잭션 매니저가 커밋이나 롤백을 하면 리소스를 다 릴리즈하고 트랜잭션 동기화 매니저에도 커넥션을 제거해준다.
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());//속성을 넣어주면 된다
+
+        try{
+
+            //비즈니스 로직  ( 커넥션도 같이 넘기자.
+            Member fromMember = memberRepository.findById(fromId);              //con Argument 추가
+            Member toMember = memberRepository.findById(toId);
+
+            //계좌이체니까 일단 내돈을 깎아야지  from의 돈을 깎고 to의 돈을 올리는것
+            memberRepository.update(fromId, fromMember.getMoney() - money);     // 1번 업데이트
+            //중간에 오류케이스도 한번 만들어봤다.
+            validation(toMember);           //1번 업데이트 후 검증에 문제 생기면 2번으로 못 넘어감.
+
+            memberRepository.update(toId, toMember.getMoney() + money);     // 2번 업데이트
+
+            //위에 로직이 정상적으로 수행되었다면,
+            transactionManager.commit(status);  //성공 시, Commit 한다.
+        }catch(Exception e){
+            transactionManager.rollback(status);     //실패시 RollBack
+            throw new IllegalStateException(e);     //예외를 던진다.
+        }
+
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외가 발생");
+        }
+    }
+}
